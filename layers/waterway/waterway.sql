f9cb8597ae58304d4eafe1e9af5f1716dcad47dc
lbl <lbl@lbl-thinkpad.encara.local.ads>
2017-12-13 09:45:52 +0100

    Added missing languages

--- layers/waterway/waterway.sql
+++ layers/waterway/waterway.sql
@@ -1,66 +1,78 @@
 
 -- etldoc: ne_110m_rivers_lake_centerlines ->  waterway_z3
 CREATE OR REPLACE VIEW waterway_z3 AS (
-    SELECT geometry, 'river'::text AS class, NULL::text AS name, NULL::text AS name_en, NULL::text AS name_de, NULL::hstore AS tags
+    SELECT geometry, 'river'::text AS class, NULL::text AS name, NULL::text AS name_en, NULL::text AS name_de,
+      NULL::text AS name_fr, NULL::text AS name_it, NULL::text AS name_es, NULL::text AS name_nl, NULL::text AS name_ru,
+      NULL::hstore AS tags
     FROM ne_110m_rivers_lake_centerlines
     WHERE featurecla = 'River'
 );
 
 -- etldoc: ne_50m_rivers_lake_centerlines ->  waterway_z4
 CREATE OR REPLACE VIEW waterway_z4 AS (
-    SELECT geometry, 'river'::text AS class, NULL::text AS name, NULL::text AS name_en, NULL::text AS name_de, NULL::hstore AS tags
+    SELECT geometry, 'river'::text AS class, NULL::text AS name, NULL::text AS name_en, NULL::text AS name_de,
+      NULL::text AS name_fr, NULL::text AS name_it, NULL::text AS name_es, NULL::text AS name_nl, NULL::text AS name_ru,
+      NULL::hstore AS tags
     FROM ne_50m_rivers_lake_centerlines
     WHERE featurecla = 'River'
 );
 
 -- etldoc: ne_10m_rivers_lake_centerlines ->  waterway_z6
 CREATE OR REPLACE VIEW waterway_z6 AS (
-    SELECT geometry, 'river'::text AS class, NULL::text AS name, NULL::text AS name_en, NULL::text AS name_de, NULL::hstore AS tags
+    SELECT geometry, 'river'::text AS class, NULL::text AS name, NULL::text AS name_en, NULL::text AS name_de,
+      NULL::text AS name_fr, NULL::text AS name_it, NULL::text AS name_es, NULL::text AS name_nl, NULL::text AS name_ru,
+      NULL::hstore AS tags
     FROM ne_10m_rivers_lake_centerlines
     WHERE featurecla = 'River'
 );
 
 -- etldoc: osm_important_waterway_linestring_gen3 ->  waterway_z9
 CREATE OR REPLACE VIEW waterway_z9 AS (
-    SELECT geometry, 'river'::text AS class, name, name_en, name_de, tags FROM osm_important_waterway_linestring_gen3
+    SELECT geometry, 'river'::text AS class, name, name_en, name_de, name_fr, name_it, name_es, name_nl, name_ru, tags FROM osm_important_waterway_linestring_gen3
 );
 
 -- etldoc: osm_important_waterway_linestring_gen2 ->  waterway_z10
 CREATE OR REPLACE VIEW waterway_z10 AS (
-    SELECT geometry, 'river'::text AS class, name, name_en, name_de, tags FROM osm_important_waterway_linestring_gen2
+    SELECT geometry, 'river'::text AS class, name, name_en, name_de, name_fr, name_it, name_es, name_nl, name_ru, tags FROM osm_important_waterway_linestring_gen2
 );
 
 -- etldoc:osm_important_waterway_linestring_gen1 ->  waterway_z11
 CREATE OR REPLACE VIEW waterway_z11 AS (
-    SELECT geometry, 'river'::text AS class, name, name_en, name_de, tags FROM osm_important_waterway_linestring_gen1
+    SELECT geometry, 'river'::text AS class, name, name_en, name_de, name_fr, name_it, name_es, name_nl, name_ru, tags FROM osm_important_waterway_linestring_gen1
 );
 
 -- etldoc: osm_waterway_linestring ->  waterway_z12
 CREATE OR REPLACE VIEW waterway_z12 AS (
-    SELECT geometry, waterway AS class, name, name_en, name_de, tags FROM osm_waterway_linestring
+    SELECT geometry, waterway AS class, name, name_en, name_de, name_fr, name_it, name_es, name_nl, name_ru, tags FROM osm_waterway_linestring
     WHERE waterway IN ('river', 'canal')
 );
 
 -- etldoc: osm_waterway_linestring ->  waterway_z13
 CREATE OR REPLACE VIEW waterway_z13 AS (
-    SELECT geometry, waterway::text AS class, name, name_en, name_de, tags FROM osm_waterway_linestring
+    SELECT geometry, waterway::text AS class, name, name_en, name_de, name_fr, name_it, name_es, name_nl, name_ru, tags FROM osm_waterway_linestring
     WHERE waterway IN ('river', 'canal', 'stream', 'drain', 'ditch')
 );
 
 -- etldoc: osm_waterway_linestring ->  waterway_z14
 CREATE OR REPLACE VIEW waterway_z14 AS (
-    SELECT geometry, waterway::text AS class, name, name_en, name_de, tags FROM osm_waterway_linestring
+    SELECT geometry, waterway::text AS class, name, name_en, name_de, name_fr, name_it, name_es, name_nl, name_ru, tags FROM osm_waterway_linestring
 );
 
 -- etldoc: layer_waterway[shape=record fillcolor=lightpink, style="rounded,filled",
 -- etldoc:     label="layer_waterway | <z3> z3 |<z4_5> z4-z5 |<z6_8> z6-8 | <z9> z9 |<z10> z10 |<z11> z11 |<z12> z12|<z13> z13|<z14> z14+" ];
 
 CREATE OR REPLACE FUNCTION layer_waterway(bbox geometry, zoom_level int)
-RETURNS TABLE(geometry geometry, class text, name text, name_en text, name_de text, tags hstore) AS $$
+RETURNS TABLE(geometry geometry, class text, name text, name_en text, name_de text,
+  name_fr text, name_it text, name_es text, name_nl text, name_ru text, tags hstore) AS $$
     SELECT geometry, class,
         NULLIF(name, '') AS name,
-        COALESCE(NULLIF(name_en, ''), name) AS name_en,
-        COALESCE(NULLIF(name_de, ''), name, name_en) AS name_de,
+        COALESCE(NULLIF(name_en, ''), tags->'name:latin', name) AS name_en,
+        COALESCE(NULLIF(name_de, ''), tags->'name:latin', name) AS name_de,
+        COALESCE(NULLIF(name_fr, ''), tags->'name:latin', name) AS name_fr,
+        COALESCE(NULLIF(name_it, ''), tags->'name:latin', name) AS name_it,
+        COALESCE(NULLIF(name_es, ''), tags->'name:latin', name) AS name_es,
+        COALESCE(NULLIF(name_nl, ''), tags->'name:latin', name) AS name_nl,
+        COALESCE(NULLIF(name_ru, ''), tags->'name:latin', name) AS name_ru,
         tags
     FROM (
         -- etldoc: waterway_z3 ->  layer_waterway:z3
